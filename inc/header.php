84f5195b5a492584bfffe67655c1577ff3324fd8
Vadim Samoilenko <vadim123544@gmail.com>
2018-06-10 13:56:07 +0300

    Interactive menu

---
+++ inc/header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cupcake.com</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <!--HEADER-->
+	<div class="navbar navbar-default" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-2 col-md-2">
+	     			<div class="navbar-header">
+	     				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	     					<span class="icon-bar"></span>
+	     					<span class="icon-bar"></span>
+	     					<span class="icon-bar"></span>
+	     				</button>
+	     				<a href="#" class="navbar-brand"><img src="img/logo.png" alt="logo" class="img-responsive"></a>
+	     			</div>
+	     		</div>
+	     		<div class="col-lg-8 col-md-8">
+	     			<div class="navbar-collapse collapse">
+	     				<ul class="nav navbar-nav navbar-left">
+	     					<li><a href="index.php">Главная</a></li>
+	     					<li><a href="route.php?id=catalog">Каталог</a></li>
+	     					<li><a href="route.php?id=gallery">Работы</a></li>
+	     					<li><a href="route.php?id=delivery">Доставка</a></li>
+	     					<li><a href="route.php?id=payment">Оплата</a></li>
+	     					<li><a href="route.php?id=contacts">Контакты</a></li>
+	     				</ul>
+	     			</div>
+	     		</div>
+	     		<div class="col-lg-2 col-md-2 text-right">
+	     			<div class="header-contacts">
+	     				<p>0639489354</p>
+	     				<a href="#">Заказать обратный звонок</a>
+	     				<p><a href="#"><img src="img/facebook.png" alt="facebook"></a>
+	     				<a href="#"><img src="img/twitter.png" alt="facebook"></a>
+	     				</p>
+	     			</div>
+	     		</div>
+      		</div>
+      	</div>
+     </div>
--- index.php
+++ index.php
@@ -1,59 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Cupcake.com</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-  <!--HEADER-->
-	<div class="navbar navbar-default" role="navigation">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-2 col-md-2">
-	     			<div class="navbar-header">
-	     				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-	     					<span class="icon-bar"></span>
-	     					<span class="icon-bar"></span>
-	     					<span class="icon-bar"></span>
-	     				</button>
-	     				<a href="#" class="navbar-brand"><img src="img/logo.png" alt="logo" class="img-responsive"></a>
-	     			</div>
-	     		</div>
-	     		<div class="col-lg-8 col-md-8">
-	     			<div class="navbar-collapse collapse">
-	     				<ul class="nav navbar-nav navbar-left">
-	     					<li><a href="#header">Главная</a></li>
-	     					<li><a href="#catalog">Каталог</a></li>
-	     					<li><a href="#jobs">Работы</a></li>
-	     					<li><a href="#delivery">Доставка</a></li>
-	     					<li><a href="#payment">Оплата</a></li>
-	     					<li><a href="#contacts">Контакты</a></li>
-	     				</ul>
-	     			</div>
-	     		</div>
-	     		<div class="col-lg-2 col-md-2 text-right">
-	     			<div class="header-contacts">
-	     				<p>0639489354</p>
-	     				<a href="#">Заказать обратный звонок</a>
-	     				<p><a href="#"><img src="img/facebook.png" alt="facebook"></a>
-	     				<a href="#"><img src="img/twitter.png" alt="facebook"></a>
-	     				</p>
-	     			</div>
-	     		</div>
-      		</div>
-      	</div>
-     </div>
+<?php include_once("inc/header.php"); ?>
 	 <div class="jumbotron">
       			<div class="container">
       				<div class="header-text">
@@ -127,6 +72,19 @@
   </div>
 
    <!--END GALLERY-->
+   <!--MAKECAKE-->
+   <div class="container makecake">
+      <div class="row">
+        <div class="col-lg-8">
+            <h2 class="text-left">Выбери и закажи тортик сам</h2>
+        </div>
+        <div class="col-lg-4">
+            <button>заказать</button>
+        </div>
+      </div>
+  
+    </div> 
+   <!--END MAKECAKE-->
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery-3.3.1.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
