2a35432bc9b5cb1fbbd9f2476dfd333a031daed4
vadim <vadim123544@gmail.com>
2018-12-02 13:50:19 +0200

    Good

--- inc/catalog.php
+++ inc/catalog.php
@@ -1,7 +1,11 @@
 <?php 
  include_once("header.php");
-
-	echo 'Catalog';
-
- ?>
+?>
+<div class="container">
+	<div class="row ">
+		<div class="col-lg-12">
+			<h1 class="text-center">Каталог</h1>
+		</div>
+	</div>
+</div>
 
