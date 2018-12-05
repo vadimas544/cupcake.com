b146a167fbc57dd67e36a3e72e2af63c7f40dbcc
vadim <vadim123544@gmail.com>
2018-12-05 09:44:35 +0200

    Main page for catalog done

--- inc/catalog.php
+++ inc/catalog.php
@@ -13,12 +13,29 @@
 	<div class="row">
 		<div class="col-lg-6 ml-auto">
 			<div class="cake">
-				<a href="#"><img class="mx-auto d-block" src="img/cake_cat2.jpg" alt="facebook"></a>
+				<span class="new_text">торты</span>
+				<a href="#"><img class="mx-auto d-block" src="img/cake_cat2.jpg" alt="facebook"></a>				
 			</div>
 		</div>
 		<div class="col-lg-6 mr-auto">
 			<div class="cupcake">
-				
+				<span>капкейки</span>
+				<a href="#"><img class="mx-auto d-block" src="img/cupcake.jpg" alt="facebook"></a>
+			</div>
+		</div>
+	</div>
+	<br>
+	<div class="row">
+		<div class="col-lg-6 ml-auto">
+			<div class="honey-cake">
+				<span>пряники</span>
+				<a href="#"><img class="mx-auto d-block" src="img/honey-cake.jpg" alt="facebook"></a>
+			</div>
+		</div>
+		<div class="col-lg-6 mr-auto">
+			<div class="merenga">
+				<span>меренги</span>
+				<a href="#"><img class="mx-auto d-block" src="img/merenga.jpg" alt="facebook"></a>
 			</div>
 		</div>
 	</div>
